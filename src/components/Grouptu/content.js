ee6f57163944fa579b75168a1b39d3dc3bd68742
lhf <2274462341@qq.com>
2019-08-02 17:30:51 +0800

    police

---
+++ src/components/Grouptu/content.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+class Contentss extends Component {
+    componentDidMount() {
+        let chart = am4core.create("contentsss", am4charts.PieChart);
+
+        // Add data
+        chart.data = [{
+            "country": "Lithuania",
+            "litres": 501.9
+        }, {
+            "country": "Czechia",
+            "litres": 301.9
+        }, {
+            "country": "Ireland",
+            "litres": 201.1
+        }, {
+            "country": "Germany",
+            "litres": 165.8
+        }, {
+            "country": "Australia",
+            "litres": 139.9
+        }, {
+            "country": "Austria",
+            "litres": 128.3
+        }, {
+            "country": "UK",
+            "litres": 99
+        }, {
+            "country": "Belgium",
+            "litres": 60
+        }, {
+            "country": "The Netherlands",
+            "litres": 50
+        }];
+
+        // Add and configure Series
+        let pieSeries = chart.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "litres";
+        pieSeries.dataFields.category = "country";
+        pieSeries.slices.template.stroke = am4core.color("#fff");
+        pieSeries.slices.template.strokeWidth = 2;
+        pieSeries.slices.template.strokeOpacity = 1;
+
+        // This creates initial animation
+        pieSeries.hiddenState.properties.opacity = 1;
+        pieSeries.hiddenState.properties.endAngle = -90;
+        pieSeries.hiddenState.properties.startAngle = -90;
+    }
+    render() {
+        return (
+            <div id="contentsss" style={{ width: 500, height: 500, background: '#fff' }}></div>
+        )
+    }
+}
+export default Contentss
---
+++ src/pages/overall/system.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class System extends Component {
+    state = {}
+    render() {
+        return (
+            <div>system</div>
+        );
+    }
+}
+
+export default System;
